61ba15ab42bfb0ccbaa5e4ebac0958a4d9971eb3
Igor Silva Santos <igor.silva1@aluno.ifsp.edu.br>
2023-11-05 17:24:18 -0300

    D 0.1: Restyled home page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,7 @@
 const LeftBar = document.querySelector('.lbar')
 const buttonLbar = document.querySelector('.barButton')
+const LangPop = document.getElementById('langPop')
+let LangOpened = false
 
 function openLbar() {
     LeftBar.style.left = '0'
@@ -11,4 +13,14 @@ function closeLbar() {
     setTimeout(() => {
         buttonLbar.removeAttribute('style')
     }, 160);
+}
+
+function appearLangPop() {
+    if (!LangOpened) {
+    LangPop.style.transform = 'scaleY(1)'
+    LangOpened = true
+    } else {
+        LangPop.removeAttribute('style')
+        LangOpened = false
+    }
 }
