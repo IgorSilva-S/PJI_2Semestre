e5454ae81fc1c740a4ba44923c26f171dc2a1f28
Igor Silva Santos <igor.silva1@aluno.ifsp.edu.br>
2023-11-08 22:42:00 -0300

    D 0.3: Finalizado doacoes2.html

--- Assets/js/donateFunctions.js
+++ Assets/js/donateFunctions.js
@@ -3,3 +3,24 @@ function linkdoacao() {
         location.href = 'doacoes2.html'
     }, 300);
 }
+
+const Compname = document.getElementById('name');
+const email = document.getElementById('mail')
+const tel = document.getElementById('telephone')
+const local = document.getElementById('address')
+const money = document.getElementById('value')
+
+function donate() {
+    if (Compname.value != '' && email.value != '' && tel.value != '' && local.value != '' && money.value != '') {
+        alert(`Obrigado pela doação de R$${money.value}! O Meio Ambiente agradeçe`)
+    } else {
+        alert(`Favor, inserir todos os dados`)
+    }
+}
+
+function returnDonationsHall() {
+    document.body.removeAttribute('style')
+    document.body.style.opacity = '0'; setTimeout(() => {
+        location.href = 'doacoes.html'
+    }, 300);
+}
