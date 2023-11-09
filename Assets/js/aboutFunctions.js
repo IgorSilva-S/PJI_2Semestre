83d4354275213926c56d763da6e7e65700264904
Igor Silva Santos <igor.silva1@aluno.ifsp.edu.br>
2023-11-09 12:12:36 -0300

    Dev 0.4

---
+++ Assets/js/aboutFunctions.js
@@ -0,0 +1,7 @@
+const goals = document.getElementById('goals')
+const team = document.getElementById('team')
+const igor = document.getElementById('igor')
+const nicholas = document.getElementById('nicholas')
+function objectives() {
+
+}
