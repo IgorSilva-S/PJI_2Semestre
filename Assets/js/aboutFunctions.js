6a672e5cf14f6031dd2b2a984d21b17f2f144e43
Igor Silva Santos <igor.silva1@aluno.ifsp.edu.br>
2023-11-09 22:17:45 -0300

    D 0.4: Quase finalizada todas as páginas em Pt-Br
    Versão que será apresentada no dia 10/11 como pré-apresentação.

--- Assets/js/aboutFunctions.js
+++ Assets/js/aboutFunctions.js
@@ -2,6 +2,101 @@ const goals = document.getElementById('goals')
 const team = document.getElementById('team')
 const igor = document.getElementById('igor')
 const nicholas = document.getElementById('nicholas')
-function objectives() {
+const jussara = document.getElementById('jussara')
+const missions = document.getElementById('missions')
+const goalsN = document.getElementById('goalsName')
+const teamN = document.getElementById('teamName')
+const igorN = document.getElementById('igorName')
+const nicholasN = document.getElementById('nicholasName')
+const jussaraN = document.getElementById('jussaraName')
+const missionsN = document.getElementById('missionsName')
 
+function openGoals() {
+    goals.className = 'sectionActive'
+    goalsN.className = 'h2Active'
+    team.removeAttribute('class')
+    teamN.removeAttribute('class')
+    igor.removeAttribute('class')
+    igorN.classList.remove('intNamesActive')
+    nicholas.removeAttribute('class')
+    nicholasN.classList.remove('intNamesActive')
+    jussara.removeAttribute('class')
+    jussaraN.classList.remove('intNamesActive')
+    missions.removeAttribute('class')
+    missionsN.removeAttribute('class')
+}
+
+function openTeam() {
+    team.className = 'sectionActive'
+    teamN.className = 'h2Active'
+    goals.removeAttribute('class')
+    goalsN.removeAttribute('class')
+    igor.removeAttribute('class')
+    igorN.classList.remove('intNamesActive')
+    nicholas.removeAttribute('class')
+    nicholasN.classList.remove('intNamesActive')
+    jussara.removeAttribute('class')
+    jussaraN.classList.remove('intNamesActive')
+    missions.removeAttribute('class')
+    missionsN.removeAttribute('class')
+}
+
+function openIgor() {
+    igor.className = 'sectionActive'
+    igorN.classList.add('intNamesActive')
+    team.removeAttribute('class')
+    teamN.removeAttribute('class')
+    goals.removeAttribute('class')
+    goalsN.removeAttribute('class')
+    nicholas.removeAttribute('class')
+    nicholasN.classList.remove('intNamesActive')
+    jussara.removeAttribute('class')
+    jussaraN.classList.remove('intNamesActive')
+    missions.removeAttribute('class')
+    missionsN.removeAttribute('class')
+}
+
+function openNicholas() {
+    nicholas.className = 'sectionActive'
+    nicholasN.classList.add('intNamesActive')
+    team.removeAttribute('class')
+    teamN.removeAttribute('class')
+    goals.removeAttribute('class')
+    goalsN.removeAttribute('class')
+    igor.removeAttribute('class')
+    igorN.classList.remove('intNamesActive')
+    jussara.removeAttribute('class')
+    jussaraN.classList.remove('intNamesActive')
+    missions.removeAttribute('class')
+    missionsN.removeAttribute('class')
+}
+
+function openJussara() {
+    jussara.className = 'sectionActive'
+    jussaraN.classList.add('intNamesActive')
+    team.removeAttribute('class')
+    teamN.removeAttribute('class')
+    goals.removeAttribute('class')
+    goalsN.removeAttribute('class')
+    igor.removeAttribute('class')
+    igorN.classList.remove('intNamesActive')
+    nicholas.removeAttribute('class')
+    nicholasN.classList.remove('intNamesActive')
+    missions.removeAttribute('class')
+    missionsN.removeAttribute('class')
+}
+
+function openMissions() {
+    missions.className = 'sectionActive'
+    missionsN.className = 'h2Active'
+    team.removeAttribute('class')
+    teamN.removeAttribute('class')
+    igor.removeAttribute('class')
+    igorN.classList.remove('intNamesActive')
+    nicholas.removeAttribute('class')
+    nicholasN.classList.remove('intNamesActive')
+    jussara.removeAttribute('class')
+    jussaraN.classList.remove('intNamesActive')
+    goals.removeAttribute('class')
+    goalsN.removeAttribute('class')
 }
