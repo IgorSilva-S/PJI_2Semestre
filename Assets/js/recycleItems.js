a56c07757970211cb0f22b6242ee6959012f53a7
Igor Silva Santos <igor.silva1@aluno.ifsp.edu.br>
2023-11-18 16:04:09 -0300

    B 3.0: Atualizado o arquivo recycleItems.js

--- Assets/js/recycleItems.js
+++ Assets/js/recycleItems.js
@@ -14,7 +14,8 @@ const create = document.querySelector('[game2-creation]')
 2 - Metal;
 3 - Vidro
 4 - Pano
-5 - Papel Amassado*/
+5 - Papel Amassado
+6 - Garrafa de vidro*/
 
 tr1.addEventListener('animationiteration', () => {
     let random = Math.floor(Math.random() * 7)
@@ -37,7 +38,7 @@ tr1.addEventListener('animationiteration', () => {
     } else if (random == 5) {
         tr1.classList.add('crumpPaper')
         tr1.value = 5
-    }  else if (random == 6) {
+    } else if (random == 6) {
         tr1.classList.add('gBottle')
         tr1.value = 6
     }
@@ -59,13 +60,13 @@ tr2.addEventListener('animationiteration', () => {
     } else if (random == 3) {
         tr2.classList.add('glass')
         tr2.value = 3
-    }  else if (random == 4) {
+    } else if (random == 4) {
         tr2.classList.add('rag')
         tr2.value = 4
     } else if (random == 5) {
         tr2.classList.add('crumpPaper')
         tr2.value = 5
-    }  else if (random == 6) {
+    } else if (random == 6) {
         tr2.classList.add('gBottle')
         tr2.value = 6
     }
@@ -115,13 +116,13 @@ tr4.addEventListener('animationiteration', () => {
     } else if (random == 3) {
         tr4.classList.add('glass')
         tr4.value = 3
-    }  else if (random == 4) {
+    } else if (random == 4) {
         tr4.classList.add('rag')
         tr4.value = 4
     } else if (random == 5) {
         tr4.classList.add('crumpPaper')
         tr4.value = 5
-    }  else if (random == 6) {
+    } else if (random == 6) {
         tr4.classList.add('gBottle')
         tr4.value = 6
     }
@@ -160,7 +161,7 @@ function grabtr1() {
             slot0.classList.add('crumpPaper')
             s1fill = true
             s1fType = 6
-        }  else if (tr1.value == 6) {
+        } else if (tr1.value == 6) {
             slot0.classList.add('gBottle')
             s1fill = true
             s1fType = 7
@@ -191,7 +192,7 @@ function grabtr1() {
             slot1.classList.add('crumpPaper')
             s2fill = true
             s2fType = 6
-        }  else if (tr1.value == 6) {
+        } else if (tr1.value == 6) {
             slot1.classList.add('gBottle')
             s2fill = true
             s2fType = 7
@@ -214,7 +215,7 @@ function grabtr1() {
             slot2.classList.add('glass')
             s3fill = true
             s3fType = 4
-        }  else if (tr1.value == 4) {
+        } else if (tr1.value == 4) {
             slot2.classList.add('rag')
             s3fill = true
             s3fType = 5
@@ -222,7 +223,7 @@ function grabtr1() {
             slot2.classList.add('crumpPaper')
             s3fill = true
             s3fType = 6
-        }  else if (tr1.value == 6) {
+        } else if (tr1.value == 6) {
             slot2.classList.add('gBottle')
             s3fill = true
             s3fType = 7
@@ -245,7 +246,7 @@ function grabtr1() {
             slot3.classList.add('glass')
             s4fill = true
             s4fType = 4
-        }  else if (tr1.value == 4) {
+        } else if (tr1.value == 4) {
             slot3.classList.add('rag')
             s4fill = true
             s4fType = 5
@@ -316,7 +317,7 @@ function grabtr2() {
             slot1.classList.add('glass')
             s2fill = true
             s2fType = 4
-        }  else if (tr2.value == 4) {
+        } else if (tr2.value == 4) {
             slot1.classList.add('rag')
             s2fill = true
             s2fType = 5
@@ -347,7 +348,7 @@ function grabtr2() {
             slot2.classList.add('glass')
             s3fill = true
             s3fType = 4
-        }  else if (tr2.value == 4) {
+        } else if (tr2.value == 4) {
             slot2.classList.add('rag')
             s3fill = true
             s3fType = 5
@@ -355,7 +356,7 @@ function grabtr2() {
             slot2.classList.add('crumpPaper')
             s3fill = true
             s3fType = 6
-        }  else if (tr2.value == 6) {
+        } else if (tr2.value == 6) {
             slot2.classList.add('gBottle')
             s3fill = true
             s3fType = 7
@@ -378,7 +379,7 @@ function grabtr2() {
             slot3.classList.add('glass')
             s4fill = true
             s4fType = 4
-        }  else if (tr2.value == 4) {
+        } else if (tr2.value == 4) {
             slot3.classList.add('rag')
             s4fill = true
             s4fType = 5
@@ -393,9 +394,9 @@ function grabtr2() {
         }
     }
     tr2.style.opacity = '0'
-    }
+}
 
-    function grabtr3() {
+function grabtr3() {
     let slot0 = document.getElementById('slot0')
     let slot1 = document.getElementById('slot1')
     let slot2 = document.getElementById('slot2')
@@ -480,7 +481,7 @@ function grabtr2() {
             slot2.classList.add('glass')
             s3fill = true
             s3fType = 4
-        }  else if (tr3.value == 4) {
+        } else if (tr3.value == 4) {
             slot2.classList.add('rag')
             s3fill = true
             s3fType = 5
@@ -511,7 +512,7 @@ function grabtr2() {
             slot3.classList.add('glass')
             s4fill = true
             s4fType = 4
-        }  else if (tr3.value == 4) {
+        } else if (tr3.value == 4) {
             slot3.classList.add('rag')
             s4fill = true
             s4fType = 5
@@ -526,7 +527,7 @@ function grabtr2() {
         }
     }
     tr3.style.opacity = '0'
-    }
+}
 
 function grabtr4() {
     let slot0 = document.getElementById('slot0')
@@ -551,7 +552,7 @@ function grabtr4() {
             slot0.classList.add('glass')
             s1fill = true
             s1fType = 4
-        }  else if (tr4.value == 4) {
+        } else if (tr4.value == 4) {
             slot0.classList.add('rag')
             s1fill = true
             s1fType = 5
@@ -582,7 +583,7 @@ function grabtr4() {
             slot1.classList.add('glass')
             s2fill = true
             s2fType = 4
-        }  else if (tr4.value == 4) {
+        } else if (tr4.value == 4) {
             slot1.classList.add('rag')
             s2fill = true
             s2fType = 5
@@ -613,7 +614,7 @@ function grabtr4() {
             slot2.classList.add('glass')
             s3fill = true
             s3fType = 4
-        }   else if (tr4.value == 4) {
+        } else if (tr4.value == 4) {
             slot2.classList.add('rag')
             s3fill = true
             s3fType = 5
@@ -644,7 +645,7 @@ function grabtr4() {
             slot3.classList.add('glass')
             s4fill = true
             s4fType = 4
-        }   else if (tr4.value == 4) {
+        } else if (tr4.value == 4) {
             slot3.classList.add('rag')
             s4fill = true
             s4fType = 5
@@ -697,9 +698,58 @@ function clearSlots() {
 }
 
 function createObj() {
+    /*Tipo de lixo - definição verificação:
+    1 - Papel;
+    2 - Plastico;
+    3 - Metal;
+    4 - Vidro
+    5 - Pano
+    6 - Papel Amassado
+    7 - Garrafa de vidro*/
     create.className = 'creation'
     if (s1fill && s2fill & s3fill && s4fill) {
-        if (s1fType == 5 && s2fType == 5 && s3fType == 1 && s4fType == 6) {
+        let v1 = s1fType
+        let v2 = s2fType
+        let v3 = s3fType
+        let v4 = s4fType
+        let resultTypes = v1 + v2 + v3 + v4
+        if (resultTypes == 17) {
+            if ((v1 == 5 || v1 == 1 || v1 == 6) && (v2 == 5 || v2 == 1 || v2 == 6) && (v3 == 5 || v3 == 1 || v3 == 6) && (v4 == 5 || v4 == 1 || v4 == 6)) {
+                create.classList.add('ragDoll')
+                clearSlots()
+            } else {
+                alert(`Não há criações para os objetos selecionados`)
+            }
+        } else if (resultTypes == 16) {
+            if ((v1 == 5 || v1 == 3) && (v2 == 5 || v2 == 3) && (v3 == 5 || v3 == 3) && (v4 == 5 || v4 == 3)) {
+                create.classList.add('telephone')
+                clearSlots()
+            } else {
+                alert(`Não há criações para os objetos selecionados`)
+            }
+        } else if (resultTypes == 8) {
+            if ((v1 == 3 || v1 == 2 || v1 == 1) && (v2 == 3 || v2 == 2 || v2 == 1) && (v3 == 3 || v3 == 2 || v3 == 1) && (v4 == 3 || v4 == 2 || v4 == 1)) {
+                create.classList.add('cardboxToy')
+                clearSlots()
+            } else {
+                alert(`Não há criações para os objetos selecionados`)
+            }
+        } else if (resultTypes == 23) {
+            if ((v1 == 5 || v1 == 6 || v1 == 7) && (v2 == 5 || v2 == 6 || v2 == 7) && (v3 == 5 || v3 == 6 || v3 == 7) && (v4 == 5 || v4 == 6 || v4 == 7)) {
+                create.classList.add('glassVase')
+                clearSlots()
+            } else {
+                alert(`Não há criações para os objetos selecionados`)
+            }
+        } else if (resultTypes == 19) {
+            if ((v1 == 3 || v1 == 5 || v1 == 6) && (v2 == 3 || v2 == 5 || v2 == 6) && (v3 == 3 || v3 == 5 || v3 == 6) && (v4 == 3 || v4 == 5 || v4 == 6)) {
+                create.classList.add('tinDrum')
+                clearSlots()
+            } else {
+                alert(`Não há criações para os objetos selecionados`)
+            }
+        }
+        /*if (s1fType == 5 && s2fType == 5 && s3fType == 1 && s4fType == 6) {
             create.classList.add('ragDoll')
             clearSlots()
         } else if (s1fType == 3 && s2fType == 5 && s3fType == 3 && s4fType == 5) {
@@ -714,7 +764,7 @@ function createObj() {
         } else if (s1fType == 3 && s2fType  == 5 && s3fType == 5 && s4fType == 6) {
             create.classList.add('tinDrum')
             clearSlots()
-        }
+        }*/
         else {
             alert(`Não há criações para os objetos selecionados`)
         }
